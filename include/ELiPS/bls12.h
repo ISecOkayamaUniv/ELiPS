c493364eec46f274f984d940e4551fc675315c28
Takahashi Yuto <>
2020-01-21 16:21:26 +0900

    changed

--- include/ELiPS/bls12.h
+++
@@ -1,9 +0,0 @@
-#ifndef BLS12_H
-#define BLS12_H
-
-#include <ELiPS/scalar.h>
-#include <ELiPS/bls12_init.h>
-#include <ELiPS/bls12_test.h>
-#include <ELiPS/test.h>
-
-#endif
--- include/ELiPS/bn12.h
+++
@@ -1,9 +0,0 @@
-#ifndef BN12_H
-#define BN12_H
-
-#include <ELiPS/scalar.h>
-#include <ELiPS/bn12_init.h>
-#include <ELiPS/bn12_test.h>
-#include <ELiPS/test.h>
-
-#endif
