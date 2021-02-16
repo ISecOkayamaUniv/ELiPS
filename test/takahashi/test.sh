9d43c0c620571d49ced462d500394e7c0979f4cb
daichi <chorichoridaichi@gmail.com>
2021-02-16 10:40:18 +0900

    rm tests

--- test/takahashi/test.sh
+++
@@ -1,9 +0,0 @@
-gcc test_g1_scm.c -o test_g1_scm -lelips -lgmp
-./test_g1_scm > result_g1_test.txt
-
-gcc test_g2_scm.c -o test_g2_scm -lelips -lgmp
-./test_g2_scm > result_g2_test.txt
-
-gcc test_g3_exp.c -o test_g3_exp -lelips -lgmp
-./test_g3_exp > result_g3_test.txt
-
