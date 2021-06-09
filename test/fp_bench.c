4c5b9d91356c91ca132136eb0a1d662b3a50ba81
daichi hattori <p3q24c2o@s.okayama-u.ac.jp>
2021-06-09 15:20:39 +0900

    add fp_bench.c

---
+++ test/fp_bench.c
@@ -0,0 +1,17 @@
+#include "ELiPS/bls12.h"
+#include "ELiPS/fr.h"
+/*============================================================================*/
+/* main                                                                       */
+/*============================================================================*/
+int main(void){
+    bls12_init();
+    test_fp_montgomery(10000);
+    // g1_test(1000);
+    // g2_test(1000);
+    // g3_test(1000);
+    // g1_set_random_test(1000);
+    //debug_pairing(1);
+
+    //billinear_test();
+    return 0;
+}
