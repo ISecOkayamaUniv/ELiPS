a56e0d0fa23340716019d9875d092975cbb0ec29
daichihattori <p3q24c2o@s.okayama-u.ac.jp>
2021-10-15 16:10:41 +0900

    fix/efp2 infinity bug

--- src/efp2.c
+++ src/efp2.c
@@ -340,25 +340,26 @@ void efp2_ecd(efp2_t *ANS, efp2_t *P) {
   if (fp2_cmp_zero(&P->y) == 0) {
     ANS->infinity = 1;
     return;
+  } else {
+    efp2_set(&tmp1_efp2, P);
+
+    fp2_add(&tmp1_fp2, &tmp1_efp2.y, &tmp1_efp2.y);
+
+    fp2_inv(&tmp1_fp2, &tmp1_fp2);
+    fp2_sqr(&tmp2_fp2, &tmp1_efp2.x);
+    fp2_add(&tmp3_fp2, &tmp2_fp2, &tmp2_fp2);
+    fp2_add(&tmp2_fp2, &tmp2_fp2, &tmp3_fp2);
+    fp2_mul(&tmp3_fp2, &tmp1_fp2, &tmp2_fp2);
+
+    fp2_sqr(&tmp1_fp2, &tmp3_fp2);
+    fp2_add(&tmp2_fp2, &tmp1_efp2.x, &tmp1_efp2.x);
+    fp2_sub(&ANS->x, &tmp1_fp2, &tmp2_fp2);
+
+    fp2_sub(&tmp1_fp2, &tmp1_efp2.x, &ANS->x);
+    fp2_mul(&tmp2_fp2, &tmp3_fp2, &tmp1_fp2);
+    fp2_sub(&ANS->y, &tmp2_fp2, &tmp1_efp2.y);
+    ANS->infinity = 0;
   }
-
-  efp2_set(&tmp1_efp2, P);
-
-  fp2_add(&tmp1_fp2, &tmp1_efp2.y, &tmp1_efp2.y);
-
-  fp2_inv(&tmp1_fp2, &tmp1_fp2);
-  fp2_sqr(&tmp2_fp2, &tmp1_efp2.x);
-  fp2_add(&tmp3_fp2, &tmp2_fp2, &tmp2_fp2);
-  fp2_add(&tmp2_fp2, &tmp2_fp2, &tmp3_fp2);
-  fp2_mul(&tmp3_fp2, &tmp1_fp2, &tmp2_fp2);
-
-  fp2_sqr(&tmp1_fp2, &tmp3_fp2);
-  fp2_add(&tmp2_fp2, &tmp1_efp2.x, &tmp1_efp2.x);
-  fp2_sub(&ANS->x, &tmp1_fp2, &tmp2_fp2);
-
-  fp2_sub(&tmp1_fp2, &tmp1_efp2.x, &ANS->x);
-  fp2_mul(&tmp2_fp2, &tmp3_fp2, &tmp1_fp2);
-  fp2_sub(&ANS->y, &tmp2_fp2, &tmp1_efp2.y);
 }
 
 void efp2_ecd_lazy_montgomery(efp2_t *ANS, efp2_t *P) {
@@ -367,25 +368,26 @@ void efp2_ecd_lazy_montgomery(efp2_t *ANS, efp2_t *P) {
   if (fp2_cmp_zero(&P->y) == 0) {
     ANS->infinity = 1;
     return;
+  } else {
+    efp2_set(&tmp1_efp2, P);
+
+    fp2_add(&tmp1_fp2, &tmp1_efp2.y, &tmp1_efp2.y);
+
+    fp2_inv_lazy_montgomery(&tmp1_fp2, &tmp1_fp2);
+    fp2_sqr_lazy_montgomery(&tmp2_fp2, &tmp1_efp2.x);
+    fp2_add(&tmp3_fp2, &tmp2_fp2, &tmp2_fp2);
+    fp2_add(&tmp2_fp2, &tmp2_fp2, &tmp3_fp2);
+    fp2_mul_lazy_montgomery(&tmp3_fp2, &tmp1_fp2, &tmp2_fp2);
+
+    fp2_sqr_lazy_montgomery(&tmp1_fp2, &tmp3_fp2);
+    fp2_add(&tmp2_fp2, &tmp1_efp2.x, &tmp1_efp2.x);
+    fp2_sub(&ANS->x, &tmp1_fp2, &tmp2_fp2);
+
+    fp2_sub(&tmp1_fp2, &tmp1_efp2.x, &ANS->x);
+    fp2_mul_lazy_montgomery(&tmp2_fp2, &tmp3_fp2, &tmp1_fp2);
+    fp2_sub(&ANS->y, &tmp2_fp2, &tmp1_efp2.y);
+    ANS->infinity = 0;
   }
-
-  efp2_set(&tmp1_efp2, P);
-
-  fp2_add(&tmp1_fp2, &tmp1_efp2.y, &tmp1_efp2.y);
-
-  fp2_inv_lazy_montgomery(&tmp1_fp2, &tmp1_fp2);
-  fp2_sqr_lazy_montgomery(&tmp2_fp2, &tmp1_efp2.x);
-  fp2_add(&tmp3_fp2, &tmp2_fp2, &tmp2_fp2);
-  fp2_add(&tmp2_fp2, &tmp2_fp2, &tmp3_fp2);
-  fp2_mul_lazy_montgomery(&tmp3_fp2, &tmp1_fp2, &tmp2_fp2);
-
-  fp2_sqr_lazy_montgomery(&tmp1_fp2, &tmp3_fp2);
-  fp2_add(&tmp2_fp2, &tmp1_efp2.x, &tmp1_efp2.x);
-  fp2_sub(&ANS->x, &tmp1_fp2, &tmp2_fp2);
-
-  fp2_sub(&tmp1_fp2, &tmp1_efp2.x, &ANS->x);
-  fp2_mul_lazy_montgomery(&tmp2_fp2, &tmp3_fp2, &tmp1_fp2);
-  fp2_sub(&ANS->y, &tmp2_fp2, &tmp1_efp2.y);
 }
 /*
 void efp2_ecd_projective_lazy(efp2_projective_t *ANS,efp2_projective_t *P){
@@ -464,42 +466,43 @@ void efp2_ecd_jacobian_lazy_montgomery(efp2_jacobian_t *ANS, efp2_jacobian_t *P)
   if (fp2_cmp_zero(&P->y) == 0) {
     ANS->infinity = 1;
     return;
+  } else {
+    efp2_jacobian_set(&Pt, P);
+
+    //s
+    fp2_mul_lazy_montgomery(&tmpY2, &Pt.y, &Pt.y);
+    fp2_mul_lazy_montgomery(&tmp1, &tmpY2, &Pt.x);
+    fp2_add(&tmp1, &tmp1, &tmp1);
+    fp2_add(&s, &tmp1, &tmp1);
+
+    //m
+    fp2_add_nonmod_single(&tmp1, &Pt.x, &Pt.x);
+    fp2_add_nonmod_single(&tmp1, &tmp1, &Pt.x);
+    fp2_mul_lazy_montgomery(&m, &tmp1, &Pt.x);
+
+    //T
+    fp2_mul_lazy_montgomery(&T, &m, &m);
+    fp2_add(&tmp1, &s, &s);
+    fp2_sub(&T, &T, &tmp1);
+
+    //ANS->x
+    fp2_set(&ANS->x, &T);
+
+    //ANS->y
+    fp2_sub_nonmod_single(&tmp1, &s, &T);
+    fp2_mul_lazy_montgomery(&buf, &tmp1, &m);
+
+    fp2_mul_lazy_montgomery(&tmp1, &tmpY2, &tmpY2);
+    fp2_add(&tmp1, &tmp1, &tmp1);
+    fp2_add(&tmp1, &tmp1, &tmp1);
+    fp2_add(&tmp1, &tmp1, &tmp1);
+    fp2_sub(&ANS->y, &buf, &tmp1);
+
+    //ANS->z
+    fp2_add_nonmod_single(&tmp1, &Pt.y, &Pt.y);
+    fp2_mul_lazy_montgomery(&ANS->z, &tmp1, &Pt.z);
+    ANS->infinity = 0;
   }
-
-  efp2_jacobian_set(&Pt, P);
-
-  //s
-  fp2_mul_lazy_montgomery(&tmpY2, &Pt.y, &Pt.y);
-  fp2_mul_lazy_montgomery(&tmp1, &tmpY2, &Pt.x);
-  fp2_add(&tmp1, &tmp1, &tmp1);
-  fp2_add(&s, &tmp1, &tmp1);
-
-  //m
-  fp2_add_nonmod_single(&tmp1, &Pt.x, &Pt.x);
-  fp2_add_nonmod_single(&tmp1, &tmp1, &Pt.x);
-  fp2_mul_lazy_montgomery(&m, &tmp1, &Pt.x);
-
-  //T
-  fp2_mul_lazy_montgomery(&T, &m, &m);
-  fp2_add(&tmp1, &s, &s);
-  fp2_sub(&T, &T, &tmp1);
-
-  //ANS->x
-  fp2_set(&ANS->x, &T);
-
-  //ANS->y
-  fp2_sub_nonmod_single(&tmp1, &s, &T);
-  fp2_mul_lazy_montgomery(&buf, &tmp1, &m);
-
-  fp2_mul_lazy_montgomery(&tmp1, &tmpY2, &tmpY2);
-  fp2_add(&tmp1, &tmp1, &tmp1);
-  fp2_add(&tmp1, &tmp1, &tmp1);
-  fp2_add(&tmp1, &tmp1, &tmp1);
-  fp2_sub(&ANS->y, &buf, &tmp1);
-
-  //ANS->z
-  fp2_add_nonmod_single(&tmp1, &Pt.y, &Pt.y);
-  fp2_mul_lazy_montgomery(&ANS->z, &tmp1, &Pt.z);
 }
 void efp2_eca(efp2_t *ANS, efp2_t *P1, efp2_t *P2) {
   static efp2_t tmp1_efp2, tmp2_efp2;
@@ -518,21 +521,22 @@ void efp2_eca(efp2_t *ANS, efp2_t *P1, efp2_t *P2) {
       efp2_ecd(ANS, P1);
       return;
     }
+  } else {
+    efp2_set(&tmp1_efp2, P1);
+    efp2_set(&tmp2_efp2, P2);
+
+    fp2_sub(&tmp1_fp2, &tmp2_efp2.x, &tmp1_efp2.x);
+    fp2_inv(&tmp1_fp2, &tmp1_fp2);
+    fp2_sub(&tmp2_fp2, &tmp2_efp2.y, &tmp1_efp2.y);
+    fp2_mul(&tmp3_fp2, &tmp1_fp2, &tmp2_fp2);
+    fp2_sqr(&tmp1_fp2, &tmp3_fp2);
+    fp2_sub(&tmp2_fp2, &tmp1_fp2, &tmp1_efp2.x);
+    fp2_sub(&ANS->x, &tmp2_fp2, &tmp2_efp2.x);
+    fp2_sub(&tmp1_fp2, &tmp1_efp2.x, &ANS->x);
+    fp2_mul(&tmp2_fp2, &tmp3_fp2, &tmp1_fp2);
+    fp2_sub(&ANS->y, &tmp2_fp2, &tmp1_efp2.y);
+    ANS->infinity = 0;
   }
-
-  efp2_set(&tmp1_efp2, P1);
-  efp2_set(&tmp2_efp2, P2);
-
-  fp2_sub(&tmp1_fp2, &tmp2_efp2.x, &tmp1_efp2.x);
-  fp2_inv(&tmp1_fp2, &tmp1_fp2);
-  fp2_sub(&tmp2_fp2, &tmp2_efp2.y, &tmp1_efp2.y);
-  fp2_mul(&tmp3_fp2, &tmp1_fp2, &tmp2_fp2);
-  fp2_sqr(&tmp1_fp2, &tmp3_fp2);
-  fp2_sub(&tmp2_fp2, &tmp1_fp2, &tmp1_efp2.x);
-  fp2_sub(&ANS->x, &tmp2_fp2, &tmp2_efp2.x);
-  fp2_sub(&tmp1_fp2, &tmp1_efp2.x, &ANS->x);
-  fp2_mul(&tmp2_fp2, &tmp3_fp2, &tmp1_fp2);
-  fp2_sub(&ANS->y, &tmp2_fp2, &tmp1_efp2.y);
 }
 
 void efp2_eca_lazy_montgomery(efp2_t *ANS, efp2_t *P1, efp2_t *P2) {
@@ -552,21 +556,22 @@ void efp2_eca_lazy_montgomery(efp2_t *ANS, efp2_t *P1, efp2_t *P2) {
       efp2_ecd_lazy_montgomery(ANS, P1);
       return;
     }
+  } else {
+    efp2_set(&tmp1_efp2, P1);
+    efp2_set(&tmp2_efp2, P2);
+
+    fp2_sub(&tmp1_fp2, &tmp2_efp2.x, &tmp1_efp2.x);
+    fp2_inv_lazy_montgomery(&tmp1_fp2, &tmp1_fp2);
+    fp2_sub(&tmp2_fp2, &tmp2_efp2.y, &tmp1_efp2.y);
+    fp2_mul_lazy_montgomery(&tmp3_fp2, &tmp1_fp2, &tmp2_fp2);
+    fp2_sqr_lazy_montgomery(&tmp1_fp2, &tmp3_fp2);
+    fp2_sub(&tmp2_fp2, &tmp1_fp2, &tmp1_efp2.x);
+    fp2_sub(&ANS->x, &tmp2_fp2, &tmp2_efp2.x);
+    fp2_sub(&tmp1_fp2, &tmp1_efp2.x, &ANS->x);
+    fp2_mul_lazy_montgomery(&tmp2_fp2, &tmp3_fp2, &tmp1_fp2);
+    fp2_sub(&ANS->y, &tmp2_fp2, &tmp1_efp2.y);
+    ANS->infinity = 0;
   }
-
-  efp2_set(&tmp1_efp2, P1);
-  efp2_set(&tmp2_efp2, P2);
-
-  fp2_sub(&tmp1_fp2, &tmp2_efp2.x, &tmp1_efp2.x);
-  fp2_inv_lazy_montgomery(&tmp1_fp2, &tmp1_fp2);
-  fp2_sub(&tmp2_fp2, &tmp2_efp2.y, &tmp1_efp2.y);
-  fp2_mul_lazy_montgomery(&tmp3_fp2, &tmp1_fp2, &tmp2_fp2);
-  fp2_sqr_lazy_montgomery(&tmp1_fp2, &tmp3_fp2);
-  fp2_sub(&tmp2_fp2, &tmp1_fp2, &tmp1_efp2.x);
-  fp2_sub(&ANS->x, &tmp2_fp2, &tmp2_efp2.x);
-  fp2_sub(&tmp1_fp2, &tmp1_efp2.x, &ANS->x);
-  fp2_mul_lazy_montgomery(&tmp2_fp2, &tmp3_fp2, &tmp1_fp2);
-  fp2_sub(&ANS->y, &tmp2_fp2, &tmp1_efp2.y);
 }
 /*
 void efp2_eca_projective_lazy(efp2_projective_t *ANS,efp2_projective_t *P1,efp2_projective_t *P2){
@@ -666,62 +671,63 @@ void efp2_eca_jacobian_lazy_montgomery(efp2_jacobian_t *ANS, efp2_jacobian_t *P1
       efp2_ecd_jacobian_lazy_montgomery(ANS, P1);
       return;
     }
+  } else {
+    efp2_jacobian_set(&Pt1, P1);
+    efp2_jacobian_set(&Pt2, P2);
+
+    //U1
+    fp2_mul_lazy_montgomery(&tmpZ2, &Pt2.z, &Pt2.z);
+    fp2_mul_lazy_montgomery(&U1, &tmpZ2, &Pt1.x);
+    //fp2_printf("U1=",&U1);printf("\n");
+
+    //U2
+    fp2_mul_lazy_montgomery(&tmpZ1, &Pt1.z, &Pt1.z);
+    fp2_mul_lazy_montgomery(&U2, &tmpZ1, &Pt2.x);
+    //fp2_printf("U2=",&U2);printf("\n");
+
+    //S1
+    fp2_mul_lazy_montgomery(&tmp1, &tmpZ2, &Pt2.z);
+    fp2_mul_lazy_montgomery(&S1, &tmp1, &Pt1.y);
+    //fp2_printf("S1=",&S1);printf("\n");
+
+    //S2
+    fp2_mul_lazy_montgomery(&tmp1, &tmpZ1, &Pt1.z);
+    fp2_mul_lazy_montgomery(&S2, &tmp1, &Pt2.y);
+    //fp2_printf("S2=",&S2);printf("\n");
+
+    //H
+    //fp2_printf("U1=",&U1);printf("\n");
+    fp2_sub(&H, &U2, &U1);
+    //fp2_printf("H=",&H);printf("\n");
+
+    //r
+    fp2_sub(&r, &S2, &S1);
+    //fp2_printf("r=",&r);printf("\n");
+
+    //ANS->x
+    fp2_mul_lazy_montgomery(&tmp1, &r, &r);
+
+    fp2_mul_lazy_montgomery(&tmpH2, &H, &H);
+    fp2_mul_lazy_montgomery(&tmpH3, &tmpH2, &H);
+    fp2_sub(&tmp2, &tmp1, &tmpH3);
+
+    fp2_mul_lazy_montgomery(&tmpU1H2, &tmpH2, &U1);
+    fp2_add(&tmp1, &tmpU1H2, &tmpU1H2);
+    fp2_sub(&ANS->x, &tmp2, &tmp1);
+
+    //ANS->y
+    fp2_sub_nonmod_single(&tmp1, &tmpU1H2, &ANS->x);
+    fp2_mul_lazy_montgomery(&tmp1, &tmp1, &r);
+
+    fp2_mul_lazy_montgomery(&tmp2, &tmpH3, &S1);
+    fp2_sub(&ANS->y, &tmp1, &tmp2);
+
+    //ANS->z
+    fp2_mul_lazy_montgomery(&tmp1, &Pt1.z, &Pt2.z);
+    fp2_mul_lazy_montgomery(&ANS->z, &tmp1, &H);
+    //getchar();
+    ANS->infinity = 0;
   }
-
-  efp2_jacobian_set(&Pt1, P1);
-  efp2_jacobian_set(&Pt2, P2);
-
-  //U1
-  fp2_mul_lazy_montgomery(&tmpZ2, &Pt2.z, &Pt2.z);
-  fp2_mul_lazy_montgomery(&U1, &tmpZ2, &Pt1.x);
-  //fp2_printf("U1=",&U1);printf("\n");
-
-  //U2
-  fp2_mul_lazy_montgomery(&tmpZ1, &Pt1.z, &Pt1.z);
-  fp2_mul_lazy_montgomery(&U2, &tmpZ1, &Pt2.x);
-  //fp2_printf("U2=",&U2);printf("\n");
-
-  //S1
-  fp2_mul_lazy_montgomery(&tmp1, &tmpZ2, &Pt2.z);
-  fp2_mul_lazy_montgomery(&S1, &tmp1, &Pt1.y);
-  //fp2_printf("S1=",&S1);printf("\n");
-
-  //S2
-  fp2_mul_lazy_montgomery(&tmp1, &tmpZ1, &Pt1.z);
-  fp2_mul_lazy_montgomery(&S2, &tmp1, &Pt2.y);
-  //fp2_printf("S2=",&S2);printf("\n");
-
-  //H
-  //fp2_printf("U1=",&U1);printf("\n");
-  fp2_sub(&H, &U2, &U1);
-  //fp2_printf("H=",&H);printf("\n");
-
-  //r
-  fp2_sub(&r, &S2, &S1);
-  //fp2_printf("r=",&r);printf("\n");
-
-  //ANS->x
-  fp2_mul_lazy_montgomery(&tmp1, &r, &r);
-
-  fp2_mul_lazy_montgomery(&tmpH2, &H, &H);
-  fp2_mul_lazy_montgomery(&tmpH3, &tmpH2, &H);
-  fp2_sub(&tmp2, &tmp1, &tmpH3);
-
-  fp2_mul_lazy_montgomery(&tmpU1H2, &tmpH2, &U1);
-  fp2_add(&tmp1, &tmpU1H2, &tmpU1H2);
-  fp2_sub(&ANS->x, &tmp2, &tmp1);
-
-  //ANS->y
-  fp2_sub_nonmod_single(&tmp1, &tmpU1H2, &ANS->x);
-  fp2_mul_lazy_montgomery(&tmp1, &tmp1, &r);
-
-  fp2_mul_lazy_montgomery(&tmp2, &tmpH3, &S1);
-  fp2_sub(&ANS->y, &tmp1, &tmp2);
-
-  //ANS->z
-  fp2_mul_lazy_montgomery(&tmp1, &Pt1.z, &Pt2.z);
-  fp2_mul_lazy_montgomery(&ANS->z, &tmp1, &H);
-  //getchar();
 }
 void efp2_eca_mixture_lazy_montgomery(efp2_jacobian_t *ANS, efp2_jacobian_t *P1, efp2_jacobian_t *P2) {
   static efp2_jacobian_t Pt1, Pt2;
@@ -743,54 +749,55 @@ void efp2_eca_mixture_lazy_montgomery(efp2_jacobian_t *ANS, efp2_jacobian_t *P1,
       efp2_ecd_jacobian_lazy_montgomery(ANS, P1);
       return;
     }
+  } else {
+    efp2_jacobian_set(&Pt1, P1);
+    efp2_jacobian_set(&Pt2, P2);
+
+    //Z1Z1
+    fp2_mul_lazy_montgomery(&Z1Z1, &Pt1.z, &Pt1.z);
+
+    //U2
+    fp2_mul_lazy_montgomery(&U2, &Pt2.x, &Z1Z1);
+
+    //S2
+    fp2_mul_lazy_montgomery(&tmp1, &Z1Z1, &Pt1.z);
+    fp2_mul_lazy_montgomery(&S2, &tmp1, &Pt2.y);
+
+    //H
+    fp2_sub(&H, &U2, &Pt1.x);
+
+    //HH
+    fp2_mul_lazy_montgomery(&HH, &H, &H);
+
+    //I
+    fp2_add(&I, &HH, &HH);
+    fp2_add(&I, &I, &I);
+
+    //J
+    fp2_mul_lazy_montgomery(&J, &HH, &H);
+
+    //r
+    fp2_sub(&r, &S2, &Pt1.y);
+
+    //V
+    fp2_mul_lazy_montgomery(&V, &Pt1.x, &HH);
+
+    //X3
+    fp2_mul_lazy_montgomery(&tmp1, &r, &r);
+    fp2_add(&tmp2, &V, &V);
+    fp2_sub(&buf, &tmp1, &J);
+    fp2_sub(&ANS->x, &buf, &tmp2);
+
+    //Y3
+    fp2_sub_nonmod_single(&tmp1, &V, &ANS->x);
+    fp2_mul_lazy_montgomery(&tmp2, &tmp1, &r);
+    fp2_mul_lazy_montgomery(&tmp1, &Pt1.y, &J);
+    fp2_sub(&ANS->y, &tmp2, &tmp1);
+
+    //ANS->z
+    fp2_mul_lazy_montgomery(&ANS->z, &Pt1.z, &H);
+    ANS->infinity = 0;
   }
-
-  efp2_jacobian_set(&Pt1, P1);
-  efp2_jacobian_set(&Pt2, P2);
-
-  //Z1Z1
-  fp2_mul_lazy_montgomery(&Z1Z1, &Pt1.z, &Pt1.z);
-
-  //U2
-  fp2_mul_lazy_montgomery(&U2, &Pt2.x, &Z1Z1);
-
-  //S2
-  fp2_mul_lazy_montgomery(&tmp1, &Z1Z1, &Pt1.z);
-  fp2_mul_lazy_montgomery(&S2, &tmp1, &Pt2.y);
-
-  //H
-  fp2_sub(&H, &U2, &Pt1.x);
-
-  //HH
-  fp2_mul_lazy_montgomery(&HH, &H, &H);
-
-  //I
-  fp2_add(&I, &HH, &HH);
-  fp2_add(&I, &I, &I);
-
-  //J
-  fp2_mul_lazy_montgomery(&J, &HH, &H);
-
-  //r
-  fp2_sub(&r, &S2, &Pt1.y);
-
-  //V
-  fp2_mul_lazy_montgomery(&V, &Pt1.x, &HH);
-
-  //X3
-  fp2_mul_lazy_montgomery(&tmp1, &r, &r);
-  fp2_add(&tmp2, &V, &V);
-  fp2_sub(&buf, &tmp1, &J);
-  fp2_sub(&ANS->x, &buf, &tmp2);
-
-  //Y3
-  fp2_sub_nonmod_single(&tmp1, &V, &ANS->x);
-  fp2_mul_lazy_montgomery(&tmp2, &tmp1, &r);
-  fp2_mul_lazy_montgomery(&tmp1, &Pt1.y, &J);
-  fp2_sub(&ANS->y, &tmp2, &tmp1);
-
-  //ANS->z
-  fp2_mul_lazy_montgomery(&ANS->z, &Pt1.z, &H);
 }
 
 void efp2_scm(efp2_t *ANS, efp2_t *P, mpz_t scalar) {
@@ -800,25 +807,25 @@ void efp2_scm(efp2_t *ANS, efp2_t *P, mpz_t scalar) {
   } else if (mpz_cmp_ui(scalar, 1) == 0) {
     efp2_set(ANS, P);
     return;
-  }
+  } else {
+    efp2_t Tmp_P, Next_P;
+    efp2_init(&Tmp_P);
+    efp2_set(&Tmp_P, P);
+    efp2_init(&Next_P);
+    int i, length;
+    length = (int)mpz_sizeinbase(scalar, 2);
+    char binary[length + 1];
+    mpz_get_str(binary, 2, scalar);
 
-  efp2_t Tmp_P, Next_P;
-  efp2_init(&Tmp_P);
-  efp2_set(&Tmp_P, P);
-  efp2_init(&Next_P);
-  int i, length;
-  length = (int)mpz_sizeinbase(scalar, 2);
-  char binary[length + 1];
-  mpz_get_str(binary, 2, scalar);
-
-  efp2_set(&Next_P, &Tmp_P);
-  for (i = 1; i < length; i++) {
-    efp2_ecd(&Next_P, &Next_P);
-    if (binary[i] == '1') {
-      efp2_eca(&Next_P, &Next_P, &Tmp_P);
+    efp2_set(&Next_P, &Tmp_P);
+    for (i = 1; i < length; i++) {
+      efp2_ecd(&Next_P, &Next_P);
+      if (binary[i] == '1') {
+        efp2_eca(&Next_P, &Next_P, &Tmp_P);
+      }
     }
+    efp2_set(ANS, &Next_P);
   }
-  efp2_set(ANS, &Next_P);
 }
 
 void efp2_scm_X_jacobian_lazy_montgomery(efp2_jacobian_t *ANS, efp2_jacobian_t *P) {
